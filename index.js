f27c30b089af960eac041f064d9154b13f422812
Amjad <amjadmaqsuma@gmail.com>
2022-04-16 23:35:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((title) => {
+    const words = title.split(" ");
+    const casedword = words.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    const response = casedword.join(" ");
+    return response;
+  });
+};
+console.log(titleCased(tutorials))
